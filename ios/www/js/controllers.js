602d291cf84063e2002d13b370ac8b183af05502
b87775 <jwoods1@fullsail.edu>
2013-07-11 05:57:24 -0600

    added angularjs and bootstrap with some css edits

--- ios/www/js/controllers.js
+++ ios/www/js/controllers.js
@@ -1,7 +1,5 @@
-angular.module('App.controllers', [])
-    .controller('MainCtrl', ['$scope', function ($scope) {
-        $scope.status = "It works!";
-    }])
-    .controller('ViewCtrl', ['$scope', function ($scope) {
-        $scope.status = "Also totally works!";
-    }]);
+angular.module('App', ['ui.bootstrap'])
+    function CollapseDemoCtrl($scope) {
+  $scope.isCollapsed = false;
+}
+ 
