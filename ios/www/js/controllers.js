1ed8704307a9cf05a063ba95c604863b3af491e4
b87775 <jwoods1@fullsail.edu>
2013-07-17 12:45:11 -0600

    trying to fix instagram pull

--- ios/www/js/controllers.js
+++ ios/www/js/controllers.js
@@ -4,11 +4,10 @@ angular.module("Instagram",['ngResource']);
 function instagramCtrl($scope,$resource) {
 
   var url ="https://api.instagram.com/v1/tags/moto/media/recent?callback=?&amp;client_id=9a456cbcc7f54683a1d9931706d5a3db;min_id=10";
-    $scope.instagram = $resource(url,{q:"angularjs", callback:'JSON_CALLBACK'},
+    $scope.instagram = $resource(url,{action:'search.json',q:'angularjs', callback:'JSON_CALLBACK'},
       {get:{method:'JSONP'}});
-    $scope.instagramResult = $scope.instagram.get(function(result){
-      console.log(result.data);
-    });
+    $scope.instagramResult = $scope.instagram.get();
+    console.log($scope.instagramResult);
     }
 
 
---
+++ ios/www/js/main.js
@@ -0,0 +1,12 @@
+function iconCtrl($scope){
+  $scope.images = [
+      {image : 'img/Camera.png', description : 'Camera'},
+      {image : 'img/Clipboard.png', description : 'Capture'},
+      {image : 'img/Arrow-right.png', description : 'Accelerometer'},
+      {image : 'img/Folder.png', description : 'Storage'},
+      {image : 'img/icon.png', description : 'Geolocation'},
+      {image : 'img/Internet.png', description : 'InAppBrowser'},
+      {image : 'img/Media.png', description : 'Media'},
+      {image : 'img/Gears.png', description : 'APIs'}
+  ];
+};
